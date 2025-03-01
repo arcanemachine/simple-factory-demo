68ea311731c03cf7fe16506e75695bf3e40b8732
Nicholas Moen <arcanemachine@gmail.com>
2025-03-01 13:05:36 -0700

    Set up repo

---
+++ config/config.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :simple_factory_demo, ecto_repos: [SimpleFactoryDemo.Repo]
+
+config :simple_factory_demo, SimpleFactoryDemo.Repo,
+  database: "simple_factory_demo",
+  username: "postgres",
+  password: "your_postgres_password",
+  hostname: "localhost"
