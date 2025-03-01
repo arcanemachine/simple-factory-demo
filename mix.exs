386334616b0531f9b792fb98762a89195ea49443
Nicholas Moen <arcanemachine@gmail.com>
2025-03-01 13:05:19 -0700

    Add Ecto

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule SimpleFactoryDemo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -18,8 +19,17 @@ defmodule SimpleFactoryDemo.MixProject do
     ]
   end
 
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
+    ]
+  end
+
   defp deps do
     [
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 end
