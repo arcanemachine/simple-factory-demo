799eabefdcb47c6e484beea9602a335af2484eda
Nicholas Moen <arcanemachine@gmail.com>
2025-11-02 03:29:36 -0700

    wip: factory POC

--- test/support/factories/users.ex
+++ test/support/factories/users.ex
@@ -1,3 +1,29 @@
 defmodule SimpleFactoryDemo.Factories.Users do
-  use SimpleFactory
+  # use SimpleFactory, strategy: {SimpleFactory.Strategies.Ecto, repo: SimpleFactoryDemo.Repo}
+
+  alias SimpleFactoryDemo.Repo
+  alias SimpleFactoryDemo.Users.User
+
+  factory User do
+    name :user_factory
+
+    # repo Repo
+
+    actions [
+      build: fn attrs -> struct(User, attrs) end,
+      insert: fn attrs -> struct(User, attrs) |> User.changeset() |> Repo.insert() end,
+      # insert: fn attrs -> apply_action(:build, attrs) |> User.changeset() |> Repo.insert() end,
+      insert!: fn attrs -> struct(User, attrs) |> User.changeset() |> Repo.insert!() end
+    ]
+
+    params fn attrs -> do
+      %{
+        id: attrs[:id],
+        username: attrs[:username] || Faker.Internet.username(),
+        author: attrs[:author]
+      }
+    end
+
+    changeset [{User, :changeset, 2}]
+  end
 end
