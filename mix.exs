a9ea71d04e26b03e2fd7514b29f0d6c1e745a8e1
Nicholas Moen <arcanemachine@gmail.com>
2025-11-02 03:08:57 -0700

    Add test paths

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule SimpleFactoryDemo.MixProject do
       app: :simple_factory_demo,
       version: "0.1.0",
       elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -32,4 +33,7 @@ defmodule SimpleFactoryDemo.MixProject do
       {:postgrex, ">= 0.0.0"}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
