cc7153fb59c59ebed839b48b444b51ed025cd0a2
Nicholas Moen <arcanemachine@gmail.com>
2026-01-24 16:54:37 -0700

    wip

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,8 @@
 import Config
 
 config :factory_man_demo, FactoryManDemo.Repo,
+  database: "factory_man_demo_test",
+  username: "postgres",
+  password: "your_postgres_password",
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
