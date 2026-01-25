0ca899644a6b97efea522ccdacd87888cab864ca
Nicholas Moen <arcanemachine@gmail.com>
2026-01-24 22:20:39 -0700

    For now, just use one factory product module for simplicity

---
+++ test/support/factories.ex
@@ -0,0 +1,36 @@
+defmodule FactoryManDemo.Factories do
+  use FactoryMan, extends: FactoryManDemo.Factory
+
+  alias FactoryManDemo.Authors.Author
+  alias FactoryManDemo.Users.User
+
+  factory :user,
+          r(build: quote) do
+    def build_user(params \\ %{}) do
+      %User{
+        id: params[:id],
+        username: Map.get(params, :username, "user-#{System.os_time(:second)}"),
+        author: params[:author]
+      }
+    end
+  end
+
+  # factory(
+  #   name: :author,
+  #   build:
+  #     quote do
+  #       def build_author(params \\ %{}) do
+  #         %Author{
+  #           # Assocs
+  #           user: params[:user] || build_user(),
+
+  #           # Fields
+  #           id: params[:id],
+  #           name: Map.get(params, :name, "Some author")
+  #         }
+  #       end
+  #     end
+  # )
+
+  # Other factories...
+end
