f5dd6e7d647fa1f663ee343f0562cccb302f9efe
Nicholas Moen <arcanemachine@gmail.com>
2026-01-26 23:33:00 -0700

    wip

--- test/support/factory_man_demo/factory.ex
+++ test/support/factory_man_demo/factory.ex
@@ -6,9 +6,6 @@ defmodule FactoryManDemo.Factory do
       after_insert: &__MODULE__.after_insert_handler/1
     ]
 
-  # @doc "An example custom build handler"
-  # def before_build_handler(params), do: params |> Map.put(:id, :world)
-
   @doc "Reset all assocs so that the `struct`'s structure matches a basic database query result."
   def after_insert_handler(%_{} = struct),
     do: Ecto.reset_fields(struct, struct.__struct__.__schema__(:associations))
