dff0e8f20eae4153c16b691070db00b9e23d9474
Nicholas Moen <arcanemachine@gmail.com>
2026-02-01 15:39:00 -0700

    wip

--- test/support/factory_man_demo/factories.ex
+++ test/support/factory_man_demo/factories.ex
@@ -1,37 +1,27 @@
 defmodule FactoryManDemo.Factories do
   use FactoryMan, extends: FactoryManDemo.Factory
 
-  # alias FactoryManDemo.Authors.Author
-  # alias FactoryManDemo.Users.User
+  alias FactoryManDemo.Authors.Author
+  alias FactoryManDemo.Users.User
 
-  # factory :user, struct: User do
-  #   base_params = %{username: "user-#{System.os_time()}"}
+  deffactory user(params \\ %{}), struct: User do
+    base_params = %{username: "user-#{System.os_time()}"}
 
-  #   Map.merge(base_params, params)
-  # end
-
-  # factory :extended_user, struct: User do
-  #   base_params = %{username: Map.get(params, :username, "extended-user-#{System.os_time()}")}
-
-  #   base_params |> Map.merge(params) |> build_user_params()
-  # end
-
-  # factory Author do
-  #   base_params = %{
-  #     user: params[:user] || build_user_struct(),
-  #     name: "Some author"
-  #   }
-
-  #   Map.merge(base_params, params)
-  # end
-
-  deffactory hello(world \\ %{}) do
-    world
+    Map.merge(base_params, params)
   end
 
-  # deffactory user(params \\ %{}), struct: User do
-  #   base_params = %{username: "user-#{System.os_time()}"}
+  deffactory extended_user(params \\ %{}), struct: User do
+    base_params = %{username: Map.get(params, :username, "extended-user-#{System.os_time()}")}
 
-  #   Map.merge(base_params, params)
-  # end
+    base_params |> Map.merge(params) |> build_user_params()
+  end
+
+  deffactory author(params \\ %{}), struct: Author do
+    base_params = %{
+      user: params[:user] || build_user_struct(),
+      name: "Some author"
+    }
+
+    Map.merge(base_params, params)
+  end
 end
