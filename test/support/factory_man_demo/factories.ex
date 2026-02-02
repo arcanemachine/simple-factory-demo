1e0698d3482abff88200933124ee9a6623bdff9a
Nicholas Moen <arcanemachine@gmail.com>
2026-02-02 02:00:27 -0700

    wip

--- test/support/factory_man_demo/factories.ex
+++ test/support/factory_man_demo/factories.ex
@@ -19,18 +19,21 @@ defmodule FactoryManDemo.Factories do
     Map.merge(base_params, params)
   end
 
+  # Embedded schema (no insert)
   deffactory embedded_schema(params \\ %{}), struct: EmbeddedSchema do
     base_params = %{some_field: "some value"}
 
     Map.merge(base_params, params)
   end
 
+  # Factory that extends another factory
   deffactory extended_user(params \\ %{}), struct: User do
     base_params = %{username: Map.get(params, :username, "extended-user-#{System.os_time()}")}
 
     base_params |> Map.merge(params) |> build_user_params()
   end
 
+  # Factory that has a nested factory
   deffactory author(params \\ %{}), struct: Author do
     base_params = %{
       user: params[:user] || build_user_struct(),
