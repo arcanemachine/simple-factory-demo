f7385c6924bd3dadc6db3d2581cb99e488ffaa4c
Nicholas Moen <arcanemachine@gmail.com>
2026-02-07 03:05:42 -0700

    wip

--- test/factory_man_demo/factories_test.exs
+++ test/factory_man_demo/factories_test.exs
@@ -198,6 +198,31 @@ defmodule FactoryManDemo.FactoriesTest do
     assert params.hello == :world
   end
 
+  # Factory option inheritance
+  test "factory options cascade from parent to child to individual factory" do
+    # Parent factory (FactoryManDemo.Factory) has repo and hooks
+    parent_opts = FactoryManDemo.Factory._factory_opts()
+    assert Keyword.has_key?(parent_opts, :repo)
+    assert Keyword.has_key?(parent_opts, :hooks)
+
+    # Child factory inherits parent's repo and hooks
+    child_opts = Factories._factory_opts()
+    assert Keyword.get(child_opts, :repo) == FactoryManDemo.Repo
+    assert Keyword.has_key?(child_opts, :hooks)
+    assert Keyword.get(child_opts, :extends) == FactoryManDemo.Factory
+
+    # Individual factory inherits all and adds its own options
+    user_opts = Factories._user_factory_opts()
+    assert Keyword.get(user_opts, :repo) == FactoryManDemo.Repo
+    assert Keyword.has_key?(user_opts, :hooks)
+    assert Keyword.get(user_opts, :struct) == FactoryManDemo.Users.User
+
+    # Factory-level options override inherited ones
+    non_insertable_opts = Factories._non_insertable_factory_opts()
+    assert Keyword.get(non_insertable_opts, :insert?) == false
+    assert Keyword.get(non_insertable_opts, :repo) == FactoryManDemo.Repo
+  end
+
   # Params-only struct factory (has struct option so it creates struct builder)
   test "params_only factory has both params and struct builders" do
     # Params builder works
